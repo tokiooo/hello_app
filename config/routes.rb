8c31683114b864d4e70e8af24f2c258a0ddab410
Tokio Miura <tmiura411@gmail.com>
2019-11-30 17:57:22 +0900

    Initialize repository

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root 'application#goodbye'
+
+  # get '/'と同義。簡略化した書き方
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
+
